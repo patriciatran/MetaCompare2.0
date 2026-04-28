a2bd5a81e8818f2eadbc03283caa3a521b3771a7
Patricia Tran <ptran5@wisc.edu>
2026-04-28 11:42:45 -0500

    change default DB path

--- mmseq.sh
+++ mmseq.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-DB='metacmpDB/GTDB/gtdb'
+# Check if the correct number of arguments is provided
+if [ "$#" -lt 4 ]; then
+    echo "Usage: $0 <input_fasta> <output_dir> <tsv_name> <db_path>"
+    exit 1
+fi
 
-mmseqs createdb ${1} ${2}sample.contigs
-mmseqs taxonomy ${2}sample.contigs $DB ${2}sample.assignments ${2}sample.tmpFolder --tax-lineage 1 --majority 0.7 --vote-mode 1 --lca-mode 3 --orf-filter 1
-mmseqs createtsv ${2}sample.contigs ${2}sample.assignments ${2}${3}
-#rm ${2}sample.contigs*
-#rm ${2}sample.assignments*
-#rm -r ${2}sample.tmpFolder
+INPUT=$1
+OUTDIR=$2
+TSV_NAME=$3
+DB=$4  # Now passed as an argument instead of hardcoded
+
+mmseqs createdb "$INPUT" "${OUTDIR}sample.contigs"
+
+mmseqs taxonomy "${OUTDIR}sample.contigs" "$DB" "${OUTDIR}sample.assignments" "${OUTDIR}sample.tmpFolder" \
+    --tax-lineage 1 --majority 0.7 --vote-mode 1 --lca-mode 3 --orf-filter 1
+
+mmseqs createtsv "${OUTDIR}sample.contigs" "${OUTDIR}sample.assignments" "${OUTDIR}${TSV_NAME}"
+
+# Optional: Clean up
+# rm "${OUTDIR}sample.contigs"*
+# rm "${OUTDIR}sample.assignments"*
+# rm -r "${OUTDIR}sample.tmpFolder"
+
